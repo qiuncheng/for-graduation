c05617f8c7e3dd0b8127f526c0904ae238b7a95d
qiuncheng <qiuncheng@gmail.com>
2017-03-13 20:05:06 +0800

    初始化项目

---
+++ SpeedRecognition/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  SpeedRecognition
+//
+//  Created by yolo on 2017/3/13.
+//  Copyright © 2017年 Qiun Cheng. All rights reserved.
+//
+
+import UIKit
+
+struct Config {
+    static let appID = "58c4ccb9"
+}
