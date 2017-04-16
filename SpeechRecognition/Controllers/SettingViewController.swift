cff25400dbbe808499a343bf09946f39e044e64b
qiuncheng <qiuncheng@gmail.com>
2017-04-16 23:23:39 +0800

    Update 添加设置

--- SpeechRecognition/Controllers/SettingViewController.swift
+++ SpeechRecognition/Controllers/SettingViewController.swift
@@ -7,17 +7,57 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class SettingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      view.backgroundColor = UIColor.white
-      title = "设置"
+class SettingViewController: UIViewController, UITableViewDelegate {
+  
+  fileprivate weak var tableView: UITableView?
+  fileprivate var inputWordObservable = Variable.init(["上传词表"])
+  fileprivate let disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.white
+    title = "设置"
+    let tableView = UITableView.init(frame: CGRect.zero, style: .grouped)
+      .then {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "SettingCell")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    view.addSubview(tableView)
+    
+    tableView.do {
+      $0.snp.makeConstraints({ [unowned self] (make) in
+        make.edges.equalTo(self.view)
+      })
+    }
+    
+    inputWordObservable
+      .asObservable()
+      .bindTo(tableView.rx.items(cellIdentifier: "SettingCell", cellType: UITableViewCell.self))  { (row, element, cell) in
+        cell.textLabel?.text = element
+      }
+      .addDisposableTo(disposeBag)
+    
+    tableView.rx
+      .setDelegate(self)
+      .addDisposableTo(disposeBag)
+    
+    tableView.rx
+      .itemSelected
+      .map({ [unowned self] indexPath in
+        return (indexPath, self.inputWordObservable.value[indexPath.row])
+      })
+      .subscribe(onNext: { (indexPath, str) in
+        if indexPath.section == 0 && indexPath.row == 0 {
+          
+        }
+      })
+      .addDisposableTo(disposeBag)
+    
   }
-
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
 }
